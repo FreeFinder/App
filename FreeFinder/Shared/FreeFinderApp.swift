06457bcbe7316bcf2239f9a14f0c425738cb9c22
jorlab1 <67760788+jorlab1@users.noreply.github.com>
2022-10-31 14:53:18 -0500

    Add files via upload

---
+++ FreeFinder/Shared/FreeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeFinderApp.swift
+//  Shared
+//
+//  Created by Jordan Labuda on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct FreeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
