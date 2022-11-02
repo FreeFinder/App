e0f3aaf4170b6792586cdd8318019fdb8665d52e
Ellen Chen <ellenrchen8@gmail.com>
2022-11-01 22:14:15 -0500

    ui tests

---
+++ FreeFinder/Shared/ListView.swift
@@ -0,0 +1,24 @@
+//
+//  ListView.swift
+//  FreeFinderTests
+//
+//  Created by Ellen Chen on 11/1/22.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    func ListClick(){
+        
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
---
+++ FreeFinder/Shared/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  FreeFinderTests
+//
+//  Created by Ellen Chen on 11/1/22.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    func annotClick(){
+        
+    }
+
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
